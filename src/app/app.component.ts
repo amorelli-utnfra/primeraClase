8780f5520ce91396a714fe1ccc8e6097dc313002
Augusto Morelli <augusto.morelli@moovingtech.com>
2025-04-01 22:17:02 -0300

    agregamos ngModel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,19 @@
 import { Component } from '@angular/core';
 import { MisDatosComponent } from './componentes/mis-datos/mis-datos.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [MisDatosComponent],
+  imports: [MisDatosComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
 
   title: string = "";
+
+  valorInput: string = "";
 
   constructor() {
 
